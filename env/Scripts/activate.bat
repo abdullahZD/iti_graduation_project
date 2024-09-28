f631318e78e4118fa5f288d39135812043788ee5
abdullahZD <abdullahmohamedhz@gmail.com>
2024-09-28 11:19:05 +0300

    first commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Cairo Lap\Desktop\lms\env
+set VIRTUAL_ENV=C:\Users\Cairo Lap\Desktop\X-Courses\env
 
 if not defined PROMPT set PROMPT=$P$G
 
